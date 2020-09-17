2e1c70bbf24394d02abb8fcf7d7dd1f214551352
sgwr-0113 <yuki.sugawara@di-v.co.jp>
2020-09-17 19:00:22 +0900

    user-validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  
+  validates :nickname, presence: true, uniqueness: {case_sensitive: true}
+  validates :birth_date, presence: true
+  validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i}
+
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]\z/} do
+  validates :first_name
+  validates :last_name
+  end
+
+  with_options presence: true, format: {with: /\A([ァ-ン]|ー)+$\z/} do
+  validates :first_name_kana
+  validates :last_name_kana
+  end
 end
