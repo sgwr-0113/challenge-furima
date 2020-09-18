c4d748a684528b2af7a1e90f8a91a1c8cf4c535d
sgwr-0113 <yuki.sugawara@di-v.co.jp>
2020-09-18 10:27:46 +0900

    items controller, Item modelの生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_065720) do
+ActiveRecord::Schema.define(version: 2020_09_18_011727) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "price", null: false
+    t.text "info", null: false
+    t.integer "scheduled_delivery_id", null: false
+    t.integer "shipping_fee_status_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "sales_status_id", null: false
+    t.integer "category_id", null: false
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", default: "", null: false
