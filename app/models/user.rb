c2347d486cd917923a3be186391b0fd96523f25f
sgwr-0113 <yuki.sugawara@di-v.co.jp>
2020-09-18 15:54:09 +0900

    devise view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,12 @@ class User < ApplicationRecord
   validates :birth_date, presence: true
   validates :password, presence: true, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}+\z/i}
 
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]\z/} do
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]+\z/} do
   validates :first_name
   validates :last_name
   end
 
-  with_options presence: true, format: {with: /\A([ァ-ン]|ー)+$\z/} do
+  with_options presence: true, format: {with: /\A([ァ-ン]|ー)+\z/} do
   validates :first_name_kana
   validates :last_name_kana
   end
