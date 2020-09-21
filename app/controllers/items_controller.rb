071504f46a8ae0461ff9302314df5f88c63cb602
sgwr-0113 <yuki.sugawara@di-v.co.jp>
2020-09-21 21:13:00 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,27 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.requre(:item).permit(
+      :image,
+      :name,
+      :price,
+      :info,
+      :scheduled_delivery_id,
+      :shipping_fee_status_id,
+      :prefecture_id,
+      :sales_status_id,
+      :category_id      
+    ).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  has_one_attached :image
+
+  belongs_to_active_hash :scheduled_delivery
+  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :sales_status
+  belongs_to_active_hash :category
+
+  with_options presence: true do
+    validates :name
+    validates :price
+    validates :info
+    validates :image
+  end
+
+  validates :name,
+  :price,
+  :info,
+  :scheduled_delivery_id,
+  :shipping_fee_status_id,
+  :prefecture_id,
+  :sales_status_id,
+  :category_id
 end
