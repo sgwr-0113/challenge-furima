bf5e29eeeeace7016412e6c8c60a462846718784
sgwr-0113 <yuki.sugawara@di-v.co.jp>
2020-09-28 15:03:07 +0900

    update item_price.js

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,17 +1,19 @@
 window.addEventListener("DOMContentLoaded", ()=> {
 
-  const priceInput = document.getElementById("item-price");
-  const addTaxDom = document.getElementById("add-tax-price");
-  const profitDom = document.getElementById("profit");
+  if (location.pathname === "/items/new" || "/items/edit") {
+    console.log(location.pathname);
+    const priceInput = document.getElementById("item-price");
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
 
-  priceInput.addEventListener("input", () => {
-    const inputRegex = /^([1-9]\d*|0)$/;
-    const inputValue = document.getElementById("item-price").value;
+    priceInput.addEventListener("input", () => {
+      const inputRegex = /^([1-9]\d*|0)$/;
+      const inputValue = document.getElementById("item-price").value;
 
-    if (inputRegex.test(inputValue)) {
-      addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
-      profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
-    } 
-  });
-
+      if (inputRegex.test(inputValue)) {
+        addTaxDom.innerHTML = Math.floor(inputValue * 0.1).toLocaleString();
+        profitDom.innerHTML = Math.floor(inputValue * 0.9).toLocaleString();
+      } 
+    });
+  }
 });
